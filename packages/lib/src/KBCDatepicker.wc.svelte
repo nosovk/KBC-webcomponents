2a2aa4d3affd0ab94dfe98cc4a702bb2ba32af0d
Taras Polishchuk <tarik@kingbillycasino.com>
2023-07-20 16:59:03 +0300

    remove selectedCountry & default value

--- packages/lib/src/KBCDatepicker.wc.svelte
+++ packages/lib/src/KBCDatepicker.wc.svelte
@@ -37,12 +37,12 @@
   export let closeOnClick = true;
   export let disabled = false;
   export let detailedValue = null;
-  export let value = "+442071838750";
+  export let value = "";
   export let searchPlaceholder = "Search";
 
   let searchText = "";
   let isOpen = false;
-  export let selectedCountry = "GB";
+  export let selectedCountry = "";
   export let valid = true;
   export let options = { invalidateOnCountryChange: true };
 
