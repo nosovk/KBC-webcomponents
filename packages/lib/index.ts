0f20782671049cd3be3f46a137f8eec2e95040a0
Taras Polishchuk <tarik@kingbillycasino.com>
2023-09-25 22:12:43 +0300

    added new iteration of the input for the new lps

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,1 +1,3 @@
 export { default as KBCDatepicker } from "./src/components/KBCDatepicker.wc.svelte";
+export { default as KBCDatepickerSecond } from "./src/components/KBCDatepickerSecond.wc.svelte";
+
